cbdd5eb898ffd33e547def68aa55a30d3f152082
Victor Tudoran <33841928+VictorTudoran@users.noreply.github.com>
2019-03-06 22:41:38 -0500

    Creating js file that handles ajax call for liking tweet
    resolves issue #70 !

---
+++ woofer-theme/js/like.js
@@ -0,0 +1,45 @@
+"$ = jQuery;
+
+$(document).ready(function(){
+
+
+  /////////////////////////////////////////////////////////
+  // AJAX calls to update user meta and change btns that
+  // are displayed
+  /////////////////////////////////////////////////////////
+  $('.like').click(function(){
+    var btn = $(this);
+    var data = {
+            action : ""woof_ajax_like"",
+            postid: btn.data('postid')
+        };
+        $.ajax({
+            url : my_ajax_object.ajax_url,
+            data : data,
+            success : function(response) {
+              btn.hide();
+              btn.next().show();
+            }
+        });
+
+  });
+
+  $('.unlike').click(function(){
+    var btn = $(this);
+    var data = {
+            action : ""woof_ajax_unlike"",
+            postid: btn.data('postid')
+        };
+        $.ajax({
+            url : my_ajax_object.ajax_url,
+            data : data,
+            success : function(response) {
+              btn.hide();
+              btn.prev().show();
+            }
+        });
+
+  });
+
+});
+"
